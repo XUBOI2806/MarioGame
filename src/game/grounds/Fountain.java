385f9de0f809864023de62b4b4f17da8abc0768c
YOUR_NAME_HERE <fxuu0010@student.monash.edu>
2022-05-21 21:48:20 +1000

    Adding integer replenish age and capacity adding another condition checking if the actor has a bottle for whether the fillbottleaction can be used Adding a getWater method which checks the capacity of the fountain and adds water to bottle if there is any. Or else nothing will be excecuted. A string will also be returned from this which will be displayed from the I/O. A get Fountain description method was added for the names of the fountain A tick method was added to refill the fountain when it has been exhausted

--- src/game/grounds/Fountain.java
+++ src/game/grounds/Fountain.java
@@ -4,11 +4,17 @@ import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.BottleManager;
 import game.actions.FillBottleAction;
 import game.actors.Drinker;
 import game.actors.Status;
+import game.items.Utils;
 
 public abstract class Fountain extends Ground {
+
+    private int replenish_age;
+
+    private int capacity;
 
     /**
      * Constructor.
@@ -18,22 +24,49 @@ public abstract class Fountain extends Ground {
     public Fountain(char displayChar) {
         super(displayChar);
         this.addCapability(Status.FOUNTAIN);
+        this.capacity = Utils.POWER_FOUNTAIN_FULL_AMOUNT;
+        this.replenish_age = Utils.FOUNTAIN_REFILL_AGE;
     }
 
     @Override
     public ActionList allowableActions(Actor actor, Location location, String direction) {
-        if(location.containsAnActor()){
+        if(location.containsAnActor() && actor.hasCapability(Status.HAS_BOTTLE)){
             return new ActionList(new FillBottleAction(this));
-
         }
         return super.allowableActions(actor, location, direction);
     }
 
-    public void getWater(Actor actor){
+    public String getWater(Actor actor){
+        if (capacity > 0){
+            BottleManager.getInstance().addWater(actor, this);
+            this.capacity -= 1;
+            return actor + " refills bottle with " + this.getWaterDescription();
+        }
+        return getFountainDescription() + " is being replenished";
     }
 
     public void buff(Drinker actor) {
     }
 
     public abstract String getWaterDescription();
+
+    public abstract String getFountainDescription();
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        if (this.capacity == 0){
+            if (this.replenish_age == 0){
+                this.capacity = Utils.POWER_FOUNTAIN_FULL_AMOUNT;
+                this.replenish_age = Utils.FOUNTAIN_REFILL_AGE;
+            }
+            else{
+                this.replenish_age -= 1;
+            }
+        }
+    }
+
+    public String getCapacity(){
+        return "(" + this.capacity + "/" + Utils.POWER_FOUNTAIN_FULL_AMOUNT + ")";
+    }
 }
