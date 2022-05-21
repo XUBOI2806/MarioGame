0cd34006c8b8017d68594ebe905d770a6d37559c
YOUR_NAME_HERE <fxuu0010@student.monash.edu>
2022-05-21 21:42:08 +1000

    Changing the fill bottle excute action method to return the string determined from the conditions within fountain. Menu description was also changed so it displays the amount of water left

--- src/game/actions/FillBottleAction.java
+++ src/game/actions/FillBottleAction.java
@@ -3,11 +3,9 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
-import edu.monash.fit2099.engine.positions.Ground;
 import game.BottleManager;
 import game.grounds.Fountain;
 import game.items.Utils;
-import game.items.Water;
 
 public class FillBottleAction extends Action {
     private Fountain fountain;
@@ -21,12 +19,11 @@ public class FillBottleAction extends Action {
         if (BottleManager.getInstance().length(actor) == Utils.BOTTLE_AMOUNT){
             return "Bottle is full";
         }
-        BottleManager.getInstance().addWater(actor,fountain);
-        return menuDescription(actor);
+        return fountain.getWater(actor);
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " refills bottle from " + fountain.getWaterDescription();
+        return actor + " refills bottle from " + fountain.getFountainDescription() + fountain.getCapacity();
     }
 }
