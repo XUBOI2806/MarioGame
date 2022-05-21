13ffee7e6022e598d0b719c628d7bbac370b2430
YOUR_NAME_HERE <fxuu0010@student.monash.edu>
2022-05-21 21:38:08 +1000

    Adding more actions to remove item from the giver in the obtainedby method

--- src/game/items/Bottle.java
+++ src/game/items/Bottle.java
@@ -1,14 +1,9 @@
 package game.items;
 
-import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
-import game.actions.FillBottleAction;
 import game.actors.Status;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
 
 
 /**
@@ -22,11 +17,14 @@ public class Bottle extends Item implements ObtainAble{
      */
     public Bottle() {
         super("Bottle", 'B', Boolean.parseBoolean("False"));
-        this.addCapability(Status.HAS_BOTTLE);
     }
 
     @Override
-    public String obtainedBy(Actor actor) {
+    public String obtainedBy(Actor actor, Actor target) {
+        target.removeItemFromInventory(this);
+        target.removeCapability(Status.HAS_BOTTLE);
+        actor.addItemToInventory(this);
+        actor.addCapability(Status.HAS_BOTTLE);
         return actor + "obtained bottle";
     }
 
